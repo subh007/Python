a54117cc1778aa3fb03718eb5879bfd98a40f300
subhash <subhash@subhash-Lenovo-G570.(none)>
2012-09-11 02:55:02 +0530

    comments added

--- download.py
+++ download.py
@@ -1,8 +1,12 @@
 import urllib
 import sys
+
 sys.path.append('/home/subhash/subh/python/requests')
+#adding the requests http://docs.python-requests.org/en/latest/index.html
 import requests
 print sys.argv[1]
+# url to download
 r=requests.get(sys.argv[1])
 with open(sys.argv[2],"wb") as code:
+#argv[2] is target file name
  code.write(r.content)
