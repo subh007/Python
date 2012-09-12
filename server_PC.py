df30fe43603c82b5cdd89a5494cd5a321e9baea0
subhash <subhash@subhash-Lenovo-G570.(none)>
2012-09-13 01:55:57 +0530

    Comments added, enhanced server to download the .html,.pdf etc., on passing the url, target file name retrieved from url.

--- server_PC.py
+++ server_PC.py
@@ -1,7 +1,13 @@
+#method returning the file name e.g.  http://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjectiveC/ObjC.pdf will return objC.pdf
+def targetname(url):
+  return url.split('/')[-1]
+
 import sys
 import socket
 sys.path.append('/home/subhash/subh/python/requests')
 import requests
+
+
 s=socket.socket()
 hostname=socket.gethostname()
 port=9595
@@ -10,10 +16,12 @@ s.listen(5)
 while True:
     print 'server started'
     conn,addr=s.accept()
-    data=conn.recv(1024)
-    print(data)
-    r=requests.get(data)
-    with open('downloded.pdf',"wb") as code:
+    url=conn.recv(1024)
+    print(url)
+# request is used to download the url (http://docs.python-requests.org/en/latest/index.html)
+    r=requests.get(url)
+    targetfile=targetname(url)
+    with open(targetfile,"wb") as code:  # writing to target file
      code.write(r.content);
      print 'page downloaded'
     conn.close()
